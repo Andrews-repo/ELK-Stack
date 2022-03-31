58ab38b40d4e163a26a958f22a47960aab6ee74e
Andrews-repo <andrewpatty3@gmail.com>
2022-03-31 09:05:24 -0700

    fixed 'command not found' error

--- bake-push.sh
+++ bake-push.sh
@@ -3,7 +3,7 @@ AWS_ACCOUNT_ID=037399687915
 REPO_NAME=elasticsearch
 ES_VERSION=6.5.4
 REGION=us-east-1
-$(aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com)
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 docker build --build-arg ES_VERSION=$ES_VERSION -t $REPO_NAME:$ES_VERSION .
 docker tag $REPO_NAME:$ES_VERSION $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$ES_VERSION
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:$ES_VERSION
